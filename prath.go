ff10bfde45d260c86f083e339221dce2758b2afd
Paul Crane <paul@acheronsoft.com>
2022-12-03 01:04:47 -0600

    Add support to displaying duplicate paths

--- prath.go
+++ prath.go
@@ -10,22 +10,32 @@ import (
 	"github.com/jedib0t/go-pretty/v6/table"
 )
 
-// FIXME: Highlight duplicates
 func getPaths() {
 	path, variableExists := os.LookupEnv("PATH")
 
 	if variableExists {
 		paths := filepath.SplitList(path)
 		pathDataTable := table.NewWriter()
+		duplicatePaths := make(map[string]int)
 
 		pathDataTable.SetOutputMirror(os.Stdout)
-		pathDataTable.AppendHeader(table.Row{"Path", "Exists"})
+		pathDataTable.AppendHeader(table.Row{"Path", "Exists", "Duplicate"})
 
 		for _, path := range paths {
+			_, exist := duplicatePaths[path]
+			duplicatePath := "No"
+
+			if exist {
+				duplicatePath = color.YellowString("Yes")
+			} else {
+				duplicatePath = "No"
+				duplicatePaths[path] = 1
+			}
+
 			if _, err := os.Stat(path); err == nil {
-				pathDataTable.AppendRow(table.Row{path, "Yes"})
+				pathDataTable.AppendRow(table.Row{path, "Yes", duplicatePath})
 			} else if os.IsNotExist(err) {
-				pathDataTable.AppendRow(table.Row{color.RedString(path), color.RedString("No")})
+				pathDataTable.AppendRow(table.Row{color.RedString(path), color.RedString("No"), duplicatePath})
 			} else {
 				fmt.Println("Error: ", err)
 			}
