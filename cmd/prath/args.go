40c84bfa9afa0ab97b5f0ddaea8ebb7dceb68185
Paul Crane <paul@acheronsoft.com>
2022-12-14 12:03:40 -0600

    Add version command line argument

--- cmd/prath/args.go
+++ cmd/prath/args.go
@@ -1,8 +1,18 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/carlmjohnson/versioninfo"
+)
+
 type ProgramArgs struct {
 }
 
 func (args ProgramArgs) Description() string {
 	return "Pretty prints a table of all directories found in $PATH environmental variable."
+}
+
+func (ProgramArgs) Version() string {
+	return fmt.Sprintln("Version: ", versioninfo.Short())
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.14
 
 require (
 	github.com/alexflint/go-arg v1.3.0
+	github.com/carlmjohnson/versioninfo v0.22.4 // indirect
 	github.com/fatih/color v1.9.0
 	github.com/jedib0t/go-pretty/v6 v6.4.3
 )
